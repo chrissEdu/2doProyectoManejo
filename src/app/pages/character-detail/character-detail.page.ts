bb5f979ad91e927f2e5d9abbe4b224fae2a032f0
Daniel Izurieta <kizurieta9883@uta.edu.ec>
2023-06-19 23:28:33 -0500

    Guardamos el valor dentro del characterID

--- src/app/pages/character-detail/character-detail.page.ts
+++ src/app/pages/character-detail/character-detail.page.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {CommonModule} from '@angular/core';
+import {IonicModule} from '@angular/core';
+import {FormsModule} from '@angular/core';
+import {ActivatedRoute} from '@angular/core';
+import {SharedModule} from '@angular/core';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+
 
 @Component({
   selector: 'app-character-detail',
   templateUrl: './character-detail.page.html',
   styleUrls: ['./character-detail.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule,SharedModule]
 })
 export class CharacterDetailPage implements OnInit {
 
-  constructor() { }
+  characterId: string ='';
+  constructor(
+    private actRoute: ActivatedRoute,
+    private rickAndMortySvc: RickAndMortyService 
+  ) { 
+    this.characterId = this.actRoute.snapshot.paramMap.get('id') as string
+    console.log(this.characterId);
+  }
 
   ngOnInit() {
   }
